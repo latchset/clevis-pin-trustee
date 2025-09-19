040802379862f4b105895d7e1f936a9943b54ef8
Jakob Naucke <jnaucke@redhat.com>
2025-09-19 10:42:06 +0200

    Add initdata parameter
    Read initdata from config, passed to trustee-attester.
    
    Signed-off-by: Jakob Naucke <jnaucke@redhat.com>

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,8 +1,10 @@
 // SPDX-FileCopyrightText: Alice Frosi <afrosi@redhat.com>
+// SPDX-FileCopyrightText: Jakob Naucke <jnaucke@redhat.com>
 //
 // SPDX-License-Identifier: MIT
 
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Server {
@@ -14,10 +16,19 @@ pub struct Server {
 pub struct Config {
     pub servers: Vec<Server>,
     pub path: String,
+    pub initdata: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Key {
     pub key_type: String,
     pub key: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+/// Sync with Trustee attestation_service::Initdata
+pub struct Initdata {
+    pub version: String,
+    pub algorithm: String,
+    pub data: HashMap<String, String>,
 }
